6d07e89e62d70ed09453c9aa1e94180ad3f5f415
Onsko <ons.kochtane@esen.tn>
2025-07-09 11:10:16 +0200

    first commit f

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}"  // ajoute jsx, ts, tsx pour React/TypeScript
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
