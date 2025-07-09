10cd6e5b7706c0418c504667c2dfd098df20c948
Onsko <ons.kochtane@esen.tn>
2025-07-09 14:38:53 +0200

    fetch user data

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import {Routes,Route} from 'react-router-dom';
+import React, { useEffect, useContext } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import EmailVerify from './pages/EmailVerify';
 import ResetPassword from './pages/ResetPassword';
-import { ToastContainer} from 'react-toastify';
+import AdminDashboard from './pages/AdminDashboard';
+import { ToastContainer } from 'react-toastify';
+import { AppContent } from './context/AppContext';
+import axios from 'axios';
 
 const App = () => {
-  //comment
-  return (
-  <div>
-    <ToastContainer/>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/email-verify' element={<EmailVerify/>}/>
-      <Route path='/reset-password' element={<ResetPassword/>}/> 
-    </Routes>
-  </div>
-  )
-}
+  const { backendUrl, setIsLoggedIn, setUserData } = useContext(AppContent);
+  const navigate = useNavigate();
 
-export default App
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const res = await axios.get(`${backendUrl}/api/user/data`, { withCredentials: true });
+
+        if (res.data.success) {
+          setUserData(res.data.user);
+          setIsLoggedIn(true);
+
+          if (res.data.user.role === 'admin') {
+            navigate('/admin');
+          } else {
+            navigate('/');
+          }
+        }
+      } catch (err) {
+        console.log("Erreur récupération des données utilisateur :", err.message);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  return (
+    <div>
+      <ToastContainer />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/email-verify' element={<EmailVerify />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+        <Route path='/admin' element={<AdminDashboard />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
